f9090aaaed5fe23aff5be4266b8c710d9f8bfb41
weimingjue <873456775@qq.com>
2021-04-25 19:21:40 +0800

    修复bug

--- src/main/kotlin/com/foundation/template/temp/ActivityAndLayout.kt
+++ src/main/kotlin/com/foundation/template/temp/ActivityAndLayout.kt
@@ -1,39 +1,52 @@
 package com.foundation.template.temp
 
-import com.android.tools.idea.wizard.template.ProjectTemplateData
-import com.android.tools.idea.wizard.template.extractLetters
-
-fun someActivity(
+fun activityTemplate(
         packageName: String,
-        entityName: String,
-        layoutName: String,
-        projectData: ProjectTemplateData
+        activityName: String,
+        layoutName: String
 ) = """
 package $packageName
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-import ${projectData.applicationPackage}.R;
-
-class ${entityName}sActivity : AppCompatActivity() {
+class ${activityName}sActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.${extractLetters(layoutName.toLowerCase())})
+        setContentView(R.layout.${layoutName})
     }
 }
 """
 
-fun someActivityLayout(
+fun manifestTemplate(
         packageName: String,
-        entityName: String) = """
-<?xml version="1.0" encoding="utf-8"?>
-<androidx.constraintlayout.widget.ConstraintLayout xmlns:android="http:<em>//</em><em>schemas.android.com/apk/res/android"</em>
-    xmlns:app="http://schemas.android.com/apk/res-auto"
-    xmlns:tools="http://schemas.android.com/tools"
-    android:layout_width="match_parent"
-    android:layout_height="match_parent"
-    tools:context="${packageName}.${entityName}sActivity">
+        activityName: String) = """
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"> 
+    <application> 
+        <activity android:name="${packageName}.${activityName}"
+            android:screenOrientation="portrait"/>
+    </application> 
+</manifest> 
+"""
 
-</androidx.constraintlayout.widget.ConstraintLayout>
+fun layoutTemplate() = """
+<?xml version="1.0" encoding="utf-8"?>
+<layout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools">
+
+    <data>
+
+        <variable
+            name="activity"
+            type="com.wang.merchant.MainActivity" />
+    </data>
+
+    <LinearLayout
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"
+        android:orientation="vertical">
+
+    </LinearLayout>
+</layout>
 """
--- src/main/kotlin/com/foundation/template/temp/Template.kt
+++ src/main/kotlin/com/foundation/template/temp/Template.kt
@@ -1,6 +1,8 @@
 package com.foundation.template.temp
 
 import com.android.tools.idea.wizard.template.*
+import com.android.tools.idea.wizard.template.impl.activities.common.addAllKotlinDependencies
+import java.io.File
 
 val mviSetupTemplate
     get() = template {
@@ -9,48 +11,53 @@ val mviSetupTemplate
         description = "创建mvp模式的Activity"
         minApi = 21
         minBuildApi = 21
-        category = Category.Other
+        category = Category.Activity
         formFactor = FormFactor.Mobile
         screens = listOf(WizardUiContext.FragmentGallery, WizardUiContext.MenuEntry,
                 WizardUiContext.NewProject, WizardUiContext.NewModule)
 
-        val packageNameParam = defaultPackageNameParameter
-        val entityName = stringParameter {
+        val activitySimpleNameParam = stringParameter {
             name = "ActivityName"
-            default = "Activity"
-            help = "创建的Activity"
-            constraints = listOf(Constraint.NONEMPTY, Constraint.ACTIVITY)
+            default = "Main"
+            help = "不用输入Activity单词"
+            constraints = listOf(Constraint.NONEMPTY, Constraint.ACTIVITY, Constraint.UNIQUE)
         }
 
-        val layoutName = stringParameter {
+        val layoutNameParam = stringParameter {
             name = "Layout Name"
-            default = "layout_"
+            default = "activity_"
             help = "创建Activity的xml资源名"
             constraints = listOf(Constraint.LAYOUT, Constraint.UNIQUE, Constraint.NONEMPTY)
-            suggest = { activityToLayout(entityName.value) }
+            suggest = { activityToLayout(activitySimpleNameParam.value) }
         }
 
-        widgets(
-                TextFieldWidget(entityName),
-                TextFieldWidget(layoutName),
-                PackageNameWidget(packageNameParam)
-        )
+        val packageNameParam = stringParameter {
+            name = "Package name"
+            visible = { !isNewModule }
+            default = "com.foundation.base"
+            constraints = listOf(Constraint.PACKAGE)
+            suggest = { packageName }
+        }
+
+        widgets(TextFieldWidget(activitySimpleNameParam),
+                TextFieldWidget(layoutNameParam),
+                PackageNameWidget(packageNameParam))
 
         recipe = { data: TemplateData ->
-            mviSetup(
-                    data as ModuleTemplateData,
-                    packageNameParam.value,
-                    entityName.value,
-                    layoutName.value
-            )
-        }
-    }
+            val moduleData = data as ModuleTemplateData
+            val packageName = packageNameParam.value
+            val asm = activitySimpleNameParam.value
+            val activityName = if (asm.endsWith("Activity")) asm else "${asm}Activity"
+            val layoutName = layoutNameParam.value
 
-val defaultPackageNameParameter
-    get() = stringParameter {
-        name = "Package name"
-        visible = { !isNewModule }
-        default = "com.foundation.base"
-        constraints = listOf(Constraint.PACKAGE)
-        suggest = { packageName }
+            val (_, srcDir, resDir, manifestDir) = moduleData
+
+            addAllKotlinDependencies(moduleData)
+
+            save(activityTemplate(packageName, activityName, layoutName), srcDir.resolve("${activityName}Activity.kt"))
+
+            mergeXml(manifestTemplate(packageName, activityName), File(manifestDir, "AndroidManifest.xml"))
+
+            save(layoutTemplate(), resDir.resolve("layout/${layoutName}.xml"))
+        }
     }
